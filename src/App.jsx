e144c14a08b3c2e44193a34c2c70c05cdfdf0ac1
walttoncoelho <walltton@outlook.com>
2022-09-02 09:53:30 -0300

    Google analiticys

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 
 
 import Home from "./Pages/Home/Home"
+import ReactGA from "react-ga4"
 
 
 function App() {
+  useEffect(() => {
+    // Initialize Google Analytics
+    ReactGA.initialize("G-2MKWE3TDRC");
+    // This just tracks a pageview
+    ReactGA.send("pageview");
+  }, []);
 
 
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { AppRouter } from './Router'
 
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
